47237fb7d5c798854bdc63d19f48772814b59e74
Robert Shearme <robert.shearme@thehutgroup.com>
2019-09-19 09:59:31 +0100

    slight mod to python for the regex

--- Python/python_solution.py
+++ Python/python_solution.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import logging
+import re
 import sys
 
 import num2word
@@ -21,25 +22,27 @@ def get_sys_argument(argument: int) -> str:
         return ""
 
 
-def read_file(file_name: str) -> list:
+def read_file(file_name: str) -> str:
     try:
         with open(file_name, 'r') as input_file:
-            return input_file.read().lower().replace('.','').split()
+            return input_file.read()
     except FileNotFoundError:
         return []
 
 
-def new_solve(file_contents: list) -> dict:
+def process_file(file_contents: str) -> list:
+    file_contents = file_contents.replace("\n", " ")
+    return re.sub(r"[^a-z0-9 ]", "", file_contents.lower()).split()
+
+def twist(file_contents: list) -> dict:
     output_dict = defaultdict(int)
     for index, word in enumerate(file_contents):
         if word in word2num:
             continue
-
         try:
             next_word = file_contents[(index+1)]
         except IndexError:
             next_word = "no multiplication"
-
         output_dict[word] += word2num.get(next_word, 1)
     return output_dict
 
@@ -56,7 +59,8 @@ def print_output(output: list, limit: int) -> None:
 
 if __name__=="__main__":
     file_name = get_sys_argument(1)
-    file_contents = read_file(file_name)
-    solution = new_solve(file_contents)
+    raw_file_contents = read_file(file_name)
+    processed_file_contents = process_file(raw_file_contents)
+    solution = twist(processed_file_contents)
     sorted_solution = sort_output(solution)
     print_output(sorted_solution, 5)
